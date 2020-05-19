6243d9368ad329f6a3e8cf14ff24518ec369ed22
xespana <xespana@risd.edu>
2020-05-19 16:24:55 -0400

    x

--- projects/thesis/script.js
+++ projects/thesis/script.js
@@ -1,5 +1,6 @@
 // fallback: show controls if autoplay fails
 // (needed for Samsung Internet for Android, as of v6.4)
+
 window.addEventListener('load', async () => {
   let video = document.querySelector('video[muted][autoplay]');
   try {
